fafbd660d2fb5e96eddbe427e4497abd5b368a10
DESKTOP-G6OECAN\Jakob <jasninus8@hotmail.dk>
2018-07-02 14:27:42 +0200

    PowerupToggle

---
+++ Assets/Scripts/PowerupToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class PowerupToggle : MonoBehaviour
+{
+    private Toggle toggle;
+
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(TogglePowerups);
+    }
+
+    public void TogglePowerups(bool toggleVal)
+    {
+        PowerupVals.shouldSpawnPowerups = toggleVal;
+        Debug.Log(PowerupVals.shouldSpawnPowerups);
+    }
+}
---
+++ Assets/Scripts/PowerupVals.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public static class PowerupVals
+{
+    public static bool shouldSpawnPowerups;
+
+    // List all powerups
+    public static bool
+        speed,
+        sizeIncrease,
+        sizeDecrease;
+}
